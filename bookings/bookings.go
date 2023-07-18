a2a3ef55f6e3ad7d35672e7f6bd509ada9a8ca94
= <=>
2023-07-18 14:03:45 +0100

    .

--- bookings/bookings.go
+++ bookings/bookings.go
@@ -1,6 +1,9 @@
 package bookings
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Bookings() {
 	conferenceName := "Go Conference"
@@ -8,33 +11,53 @@ func Bookings() {
 	var remainingTickets uint = 50
 	bookingStorage := []string{}
 
-	fmt.Printf("Conference Tickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, conferenceName, remainingTickets)
 	fmt.Printf("Welcome to %v %v\n", conferenceName, "booking application")
 	fmt.Printf("We have total of %v, tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+	for {
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - uint(userTickets)
-	bookingStorage = append(bookingStorage, firstName+" "+lastName)
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		if userTickets > remainingTickets {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
+			continue
+		}
 
-	fmt.Printf("These are all our bookings: %v\n", bookingStorage)
+		remainingTickets = remainingTickets - userTickets
+		bookingStorage = append(bookingStorage, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookingStorage {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		noTicketsRemaining := remainingTickets == 0
+
+		if noTicketsRemaining {
+			fmt.Println("Our conference is booked out. Come back next year")
+			break
+		}
+	}
+
 }
